417cf8f688b7707c00182fa2d114d8785aef450e
muheenudeen <muheenudeen313@gmail.com>
2024-10-26 16:54:10 +0530

    value

--- passByValueRef.js
+++ passByValueRef.js
@@ -15,3 +15,12 @@ console.log("Outside function:", obj.name); // Outputs: Outside function: Bob
 //value
 
 
+let x = 10;
+
+function changeValue(val) {
+    val = 20;
+    console.log("Inside function:", val); // Outputs: Inside function: 20
+}
+
+changeValue(x);
+console.log("Outside function:", x); // Outputs: Outside function: 10
